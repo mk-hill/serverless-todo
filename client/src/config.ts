9e5e277ebb339b532041ee0beb45835c93e575ac
mk-hill <40850298+mk-hill@users.noreply.github.com>
2020-05-11 19:48:05 -0500

    Update client config

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,10 +1,7 @@
-// TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = 'kuzgkl6oka'
-export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+export const apiEndpoint = `https://todo.mk-hill.com`
 
 export const authConfig = {
-  // TODO: Create an Auth0 application and copy values from it into this map
   domain: 'dev-9i08gx84.auth0.com', // Auth0 domain
   clientId: '1821cK3PViVAROO0ZFoA2uPfXGTuEMBw', // Auth0 client id
-  callbackUrl: 'http://localhost:3000/callback'
+  callbackUrl: 'http://todos.mk-hill.com/callback'
 }
